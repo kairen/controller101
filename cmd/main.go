8f0f2dedfe87158ae30e79bbdfa359a21f07d5d4
kairen <k2r2.bai@gmail.com>
2019-10-09 21:50:46 +0800

    Add leader election

--- cmd/main.go
+++ cmd/main.go
@@ -30,22 +30,34 @@ import (
 	cloudnativeinformer "github.com/cloud-native-taiwan/controller101/pkg/generated/informers/externalversions"
 	"github.com/cloud-native-taiwan/controller101/pkg/version"
 	flag "github.com/spf13/pflag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/klog"
 )
 
 const defaultSyncTime = time.Second * 30
 
 var (
-	kubeconfig  string
-	showVersion bool
-	threads     int
+	kubeconfig         string
+	showVersion        bool
+	threads            int
+	leaderElect        bool
+	id                 string
+	leaseLockName      string
+	leaseLockNamespace string
 )
 
 func parseFlags() {
 	flag.StringVarP(&kubeconfig, "kubeconfig", "", "", "Absolute path to the kubeconfig file.")
 	flag.IntVarP(&threads, "threads", "", 2, "Number of worker threads used by the controller.")
+	flag.StringVarP(&id, "holder-identity", "", os.Getenv("POD_NAME"), "the holder identity name")
+	flag.BoolVarP(&leaderElect, "leader-elect", "", true, "Start a leader election client and gain leadership before executing the main loop. ")
+	flag.StringVar(&leaseLockName, "lease-lock-name", "controller101", "the lease lock resource name")
+	flag.StringVar(&leaseLockNamespace, "lease-lock-namespace", "", "the lease lock resource namespace")
 	flag.BoolVarP(&showVersion, "version", "", false, "Display the version.")
 	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 	flag.Parse()
@@ -80,6 +92,7 @@ func main() {
 		klog.Fatalf("Error to build rest config: %s", err.Error())
 	}
 
+	k8sclientset := clientset.NewForConfigOrDie(k8scfg)
 	clientset, err := cloudnative.NewForConfig(k8scfg)
 	if err != nil {
 		klog.Fatalf("Error to build cloudnative clientset: %s", err.Error())
@@ -91,9 +104,36 @@ func main() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	if err := controller.Run(ctx, threads); err != nil {
-		klog.Fatalf("Error to run the controller instance: %s.", err)
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      leaseLockName,
+			Namespace: leaseLockNamespace,
+		},
+		Client: k8sclientset.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: id,
+		},
 	}
+
+	go leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		Lock:            lock,
+		ReleaseOnCancel: true,
+		LeaseDuration:   60 * time.Second,
+		RenewDeadline:   15 * time.Second,
+		RetryPeriod:     5 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				if err := controller.Run(ctx, threads); err != nil {
+					klog.Fatalf("Error to run the controller instance: %s.", err)
+				}
+				klog.Infof("%s: leading", id)
+			},
+			OnStoppedLeading: func() {
+				controller.Stop()
+				klog.Infof("%s: lost", id)
+			},
+		},
+	})
 
 	<-signalChan
 	cancel()
